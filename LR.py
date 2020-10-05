9598c2f95a70fc033c742dd9a606e3a2e709985b
Elias2344 <40278903+Elias2344@users.noreply.github.com>
2020-10-04 20:21:45 -0600

    Add files via upload

---
+++ LR.py
@@ -0,0 +1,19 @@
+radio = int(input("Favor de ingresar el radio de la pizza: "))
+pi = 3.1416
+area = (radio ** 2) * pi
+precio1 = 199
+total = area / precio1
+
+print("Costo de la pizza es ", total,"pesos")
+
+radio2 = int(input("Favor de ingresar el radio de la pizza: "))
+pi = 3.1415
+area = (radio ** 2) * pi
+precio2 = 279
+total = area / precio2
+
+print("Costo de la pizza es ", total,"pesos")
+
+
+
+
